2197ec07a218c260db4ed9c8694e15910107b5a6
mauricedchateau <maurice.de.chateau@gmail.com>
2013-01-14 19:00:32 +0100

    First step to a running environment: this combines EAP-6.0.1 and BRMS-5.3.1, which can be run from JBDS-5.0.1.

---
+++ init.sh
@@ -0,0 +1,92 @@
+#!/bin/bash 
+JBOSS_HOME=./target/jboss-eap-6.0
+SERVER_DIR=$JBOSS_HOME/standalone
+SRC_DIR=./installs
+EAP=jboss-eap-6.0.1.zip
+EAP_VERSION=6.0.1
+BRMS=brms-p-5.3.1.GA-deployable-ee6.zip
+BRMS_VERSION=5.3.1
+
+echo
+echo Setting up the EAP + BRMS testing environment...
+echo
+
+# make some checks first before proceeding.	
+if [[ -x $SRC_DIR/$EAP || -L $SRC_DIR/$EAP ]]; then
+	echo EAP sources are present...
+	echo
+else
+	echo Need to download $EAP package from the Customer Support Portal 
+	echo and place it in the $SRC_DIR directory to proceed...
+	echo
+	exit
+fi
+
+if [[ -x $SRC_DIR/$BRMS || -L $SRC_DIR/$BRMS ]]; then
+	echo BRMS sources are present...
+	echo
+else
+	echo Need to download $BRMS package from the Customer Support Portal 
+	echo and place it in the $SRC_DIR directory to proceed...
+	echo
+	exit
+fi
+
+# Create the target directory if it does not already exist
+if [ ! -x target ]; then
+	echo "  - creating the target directory..."
+	echo
+  mkdir target
+else
+	echo "  - detected target directory, moving on..."
+	echo
+fi
+
+# Move the old JBoss instance, if it exists, to the OLD position
+if [ -x $JBOSS_HOME ]; then
+	echo "  - existing JBoss Enterprise Application Platform $VERSION detected..."
+	echo
+	echo "  - moving existing JBoss Enterprise Application Platform $VERSION aside..."
+	echo
+  rm -rf $JBOSS_HOME.OLD
+  mv $JBOSS_HOME $JBOSS_HOME.OLD
+fi
+
+# Unzip the JBoss EAP instance
+echo Unpacking JBoss Enterprise Application Platform $EAP_VERSION...
+echo
+unzip -q -d target $SRC_DIR/$EAP
+
+# Unzip the required files from JBoss BRMS Deployable
+echo Unpacking JBoss Enterprise BRMS $BRMS_VERSION...
+echo
+
+unzip -q $SRC_DIR/$BRMS jboss-brms-manager-ee6.zip 
+echo "  - deploying JBoss Enterprise BRMS Manager WAR..."
+echo
+unzip -q -d $SERVER_DIR/deployments jboss-brms-manager-ee6.zip
+rm jboss-brms-manager-ee6.zip
+
+unzip -q $SRC_DIR/$BRMS jboss-jbpm-console-ee6.zip 
+echo "  - deploying jBPM Console WARs..."
+echo
+unzip -q -d $SERVER_DIR/deployments jboss-jbpm-console-ee6.zip
+rm jboss-jbpm-console-ee6.zip
+
+echo Rounding up, setting permissions and copying support files...
+echo
+# Set permissions for BRMS.
+cp support/standalone.xml $SERVER_DIR/configuration
+cp support/brms-roles.properties $SERVER_DIR/configuration
+cp support/brms-users.properties $SERVER_DIR/configuration
+cp support/components.xml $SERVER_DIR/deployments/jboss-brms.war/WEB-INF
+
+# Enable deployments of BRMS WARs
+cp support/jboss-brms.war.dodeploy $SERVER_DIR/deployments
+cp support/business-central.war.dodeploy $SERVER_DIR/deployments
+cp support/business-central-server.war.dodeploy $SERVER_DIR/deployments
+cp support/designer.war.dodeploy $SERVER_DIR/deployments
+cp support/jbpm-human-task.war.dodeploy $SERVER_DIR/deployments
+
+echo Integration $VERSION Home Loan Demo Setup Complete.
+echo
