7b5c3cd4f65793b23f69042d4deafb1fc86e2cf1
mauricedchateau <maurice.de.chateau@gmail.com>
2013-01-22 12:07:32 +0100

    Next step: EAR deploys and shows page, packages are fetched.

--- projects/jboss-brms-testing/brms-testing-web/src/main/java/org/jboss/brms/test/controller/TesterBean.java
+++ projects/jboss-brms-testing/brms-testing-web/src/main/java/org/jboss/brms/test/controller/TesterBean.java
@@ -2,6 +2,7 @@ package org.jboss.brms.test.controller;
 
 import java.io.IOException;
 import java.io.Serializable;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.text.MessageFormat;
@@ -15,14 +16,16 @@ import javax.inject.Inject;
 import javax.inject.Named;
 import javax.xml.bind.JAXB;
 
-import org.apache.http.client.HttpClient;
+import org.apache.commons.lang.StringUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 import org.apache.log4j.Logger;
 import org.jboss.brms.test.guvnor.Assets;
 import org.jboss.brms.test.guvnor.Packages;
-import org.richfaces.component.UISelect;
 
 @Named
 @SessionScoped
@@ -30,21 +33,24 @@ public class TesterBean implements Serializable {
     /** Serial version ID. */
     private static final long serialVersionUID = 1L;
 
-    private static final String GUVNOR_API_BASE_URL = "http://localhost:8080/jboss-brms/rest";
+    private static final String GUVNOR_API_HOST = "localhost";
+    private static final int GUVNOR_API_PORT = 8080;
+    private static final String GUVNOR_API_USER = "admin";
+    private static final String GUVNOR_API_PASSWORD = "admin";
+    private static final String GUVNOR_API_BASE_URL = "http://" + GUVNOR_API_HOST + ":" + GUVNOR_API_PORT + "/jboss-brms/rest";
     private static final String GUVNOR_API_PACKAGES_PATH = GUVNOR_API_BASE_URL + "/packages";
     private static final String GUVNOR_API_ASSETS_PATH = GUVNOR_API_PACKAGES_PATH + "/{0}/assets";
 
     @Inject
     private transient Logger log;
 
+    private SelectItem selectedPackage = new SelectItem();
+
     public List<SelectItem> getGuvnorPackages() {
         log.debug("Retrieving packages from Guvnor.");
 
         // Call Guvnor to retrieve the available packages.
-        final String responseXml = doGetCall(GUVNOR_API_PACKAGES_PATH);
-
-        // Unmarshal the response.
-        final Packages packages = JAXB.unmarshal(responseXml, Packages.class);
+        final Packages packages = getFromGuvnor(GUVNOR_API_PACKAGES_PATH, Packages.class);
 
         // Format the output.
         log.debug("Current packages in Guvnor:");
@@ -56,19 +62,24 @@ public class TesterBean implements Serializable {
         return packageNameList;
     }
 
+    public SelectItem getSelectedPackage() {
+        return selectedPackage;
+    }
+
+    public void setSelectedPackage(final SelectItem selectedPackage) {
+        System.out.println("New value for selected package: " + selectedPackage.getValue());
+        this.selectedPackage = selectedPackage;
+    }
+
     public void packageSelected(final ValueChangeEvent evt) {
-        final Object value = ((UISelect) evt.getComponent()).getValue();
-        log.info("Current value for the selected package = " + value);
+        System.out.println("Current value for the selected package = " + getSelectedPackage().getValue());
     }
 
     public List<SelectItem> getGuvnorAssets(final String packageName) {
         log.debug("Retrieving assets for package " + packageName + " from Guvnor.");
 
         // Call Guvnor to retrieve the available assets.
-        final String responseXml = doGetCall(MessageFormat.format(GUVNOR_API_ASSETS_PATH, packageName));
-
-        // Unmarshal the response.
-        final Assets assets = JAXB.unmarshal(responseXml, Assets.class);
+        final Assets assets = getFromGuvnor(MessageFormat.format(GUVNOR_API_ASSETS_PATH, packageName), Assets.class);
 
         // Format the output.
         log.debug("Current assets for package " + packageName + " in Guvnor:");
@@ -81,13 +92,19 @@ public class TesterBean implements Serializable {
         return assetNameList;
     }
 
-    private String doGetCall(final String uriString) {
-        String responseBody = null;
-        final HttpClient httpclient = new DefaultHttpClient();
+    private <T> T getFromGuvnor(final String uriString, final Class<T> clazz) {
+        // Retrieve the info from Guvnor as XML.
+        String responseXml = null;
+        final DefaultHttpClient httpclient = new DefaultHttpClient();
+        httpclient.getCredentialsProvider().setCredentials(new AuthScope(GUVNOR_API_HOST, GUVNOR_API_PORT),
+                new UsernamePasswordCredentials(GUVNOR_API_USER, GUVNOR_API_PASSWORD));
         try {
             final URI uri = new URI(uriString);
-            final HttpGet httpget = new HttpGet(uri);
-            responseBody = httpclient.execute(httpget, new BasicResponseHandler());
+            final HttpGet httpGet = new HttpGet(uri);
+            httpGet.addHeader("Accept", "application/xml");
+            final HttpResponse response = httpclient.execute(httpGet);
+            responseXml = EntityUtils.toString(response.getEntity());
+            EntityUtils.consume(response.getEntity());
         } catch (final URISyntaxException uriEx) {
             log.error("Bad URI for request.", uriEx);
         } catch (final IOException ioEx) {
@@ -97,6 +114,11 @@ public class TesterBean implements Serializable {
             httpclient.getConnectionManager().shutdown();
         }
 
-        return responseBody;
+        // Unmarshal the response.
+        T result = null;
+        if (StringUtils.isNotBlank(responseXml)) {
+            result = JAXB.unmarshal(new StringReader(responseXml), clazz);
+        }
+        return result;
     }
 }
