43575e2c3a8cbae3c69bc215f5b472d9da9ff0be
mauricedchateau <maurice.de.chateau@gmail.com>
2013-02-24 23:57:56 +0100

    Corrected use of TaskClient API.

--- projects/jboss-brms-testing/brms-testing-examples/src/main/java/org/jboss/brms/test/service/data/RewardsData.java
+++ projects/jboss-brms-testing/brms-testing-examples/src/main/java/org/jboss/brms/test/service/data/RewardsData.java
@@ -1,21 +1,28 @@
 package org.jboss.brms.test.service.data;
 
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.commons.lang.SerializationUtils;
 import org.drools.runtime.process.WorkItemManager;
 import org.jbpm.process.instance.impl.demo.SystemOutWorkItemHandler;
 import org.jbpm.task.AccessType;
+import org.jbpm.task.Status;
 import org.jbpm.task.query.TaskSummary;
 import org.jbpm.task.service.ContentData;
 import org.jbpm.task.service.TaskClient;
 import org.jbpm.task.service.responsehandlers.BlockingTaskOperationResponseHandler;
 import org.jbpm.task.service.responsehandlers.BlockingTaskSummaryResponseHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RewardsData extends AbstractDataProvider {
     private static final String PROCESS_ID = "org.jbpm.approval.rewards.extended";
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RewardsData.class);
 
     /** {@inheritDoc} */
     @Override
@@ -41,8 +48,33 @@ public class RewardsData extends AbstractDataProvider {
     public void performTasks(final TaskClient taskClient) {
         // Get task.
         final BlockingTaskSummaryResponseHandler taskSummaryHandler = new BlockingTaskSummaryResponseHandler();
-        taskClient.getTasksAssignedAsPotentialOwner("peter", "en-UK", taskSummaryHandler);
-        final TaskSummary task = taskSummaryHandler.getResults().get(0);
+        taskClient.getTasksAssignedAsPotentialOwner("Peter Parker", Arrays.asList("HR"), "en-UK", taskSummaryHandler);
+        final List<TaskSummary> taskList = taskSummaryHandler.getResults();
+        if (taskList.isEmpty()) {
+            LOGGER.error("Unable to retrieve task to be performed: aborting.");
+            return;
+        }
+        TaskSummary task = null;
+        for (final TaskSummary summary : taskList) {
+            if (Status.Ready.equals(summary.getStatus())) {
+                task = summary;
+                break;
+            }
+        }
+        if (task == null) {
+            LOGGER.error("No 'READY' tasks to be performed: aborting.");
+            return;
+        }
+
+        // Claim task.
+        BlockingTaskOperationResponseHandler taskOperationHandler = new BlockingTaskOperationResponseHandler();
+        taskClient.claim(task.getId(), "Peter Parker", Arrays.asList("HR"), taskOperationHandler);
+        taskOperationHandler.waitTillDone(1000L);
+
+        // Start task.
+        taskOperationHandler = new BlockingTaskOperationResponseHandler();
+        taskClient.start(task.getId(), "Peter Parker", taskOperationHandler);
+        taskOperationHandler.waitTillDone(1000L);
 
         // Complete task.
         final ContentData content = new ContentData();
@@ -51,8 +83,8 @@ public class RewardsData extends AbstractDataProvider {
         taskParams.put("Explanation", "Great work");
         taskParams.put("Outcome", "Approved");
         content.setContent(SerializationUtils.serialize((Serializable) taskParams));
-        final BlockingTaskOperationResponseHandler taskOperationHandler = new BlockingTaskOperationResponseHandler();
-        taskClient.complete(task.getId(), "peter", content, taskOperationHandler);
+        taskOperationHandler = new BlockingTaskOperationResponseHandler();
+        taskClient.complete(task.getId(), "Peter Parker", content, taskOperationHandler);
         taskOperationHandler.waitTillDone(1000L);
     }
 }
